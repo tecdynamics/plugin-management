6a367bb6f2d775fa9b8c5003c3ce42dea15a2fcd
Tec-Dynamics <support@tecdynamics.co.uk>
2022-02-12 22:10:12 +0000

    Add files via upload

---
+++ src/Events/ActivatedPluginEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tec\PluginManagement\Events;
+
+use Tec\Base\Events\Event;
+use Illuminate\Queue\SerializesModels;
+
+class ActivatedPluginEvent extends Event
+{
+    use SerializesModels;
+
+    /**
+     * @var string
+     */
+    public $plugin;
+
+    /**
+     * ActivatedPluginEvent constructor.
+     * @param string $plugin
+     */
+    public function __construct(string $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+}
---
+++ src/Providers/CommandServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tec\PluginManagement\Providers;
+
+use Tec\PluginManagement\Commands\PluginActivateAllCommand;
+use Tec\PluginManagement\Commands\PluginActivateCommand;
+use Tec\PluginManagement\Commands\PluginAssetsPublishCommand;
+use Tec\PluginManagement\Commands\PluginDeactivateAllCommand;
+use Tec\PluginManagement\Commands\PluginDeactivateCommand;
+use Tec\PluginManagement\Commands\PluginRemoveCommand;
+use Illuminate\Support\ServiceProvider;
+
+class CommandServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                PluginAssetsPublishCommand::class,
+            ]);
+        }
+
+        $this->commands([
+            PluginActivateCommand::class,
+            PluginDeactivateCommand::class,
+            PluginRemoveCommand::class,
+            PluginActivateAllCommand::class,
+            PluginDeactivateAllCommand::class,
+        ]);
+    }
+}
